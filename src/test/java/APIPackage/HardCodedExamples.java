28f2e6ada92b201b6733b6c1b45aca8698bf4ef2
romansahel <roman.sahel1987@gmail.com>
2023-01-30 15:30:28 -0800

    Frist Commit

---
+++ src/test/java/APIPackage/HardCodedExamples.java
@@ -0,0 +1,120 @@
+package APIPackage;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+//to change the order of execution we use fix method order since it is executing in top to bottom approach
+//which is not good for us.
+//this method sorters will execute my methods in ascending/alphabetically order
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HardCodedExamples {
+
+    //one thing to remember
+    //base URI - base URL
+    //end then using when keyword, we will send the end point
+
+    String baseURI =  RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+
+    //we need to perform CRUD operations
+    String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzQxNzM0MTgsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY3NDIxNjYxOCwidXNlcklkIjoiNTA0NSJ9.NNMwQAu3Dd_fMpwe3PWHZN87S1cY5m2rrG8W4lvZsWU";
+    static String employee_id;
+
+    @Test
+    public void bgetOneEmployee(){
+        //prepare the request
+        //to prepare the request, we use request specification
+        RequestSpecification request = given().header("Authorization", token)
+                .header("Content-Type", "application/json")
+                .queryParam("employee_id", employee_id);
+
+        //to hit the end point/ to make the request which will return response
+        Response response = request.when().get("/getOneEmployee.php");
+
+        // System.out.println(response.asString());
+        response.prettyPrint();
+        //verifying the status code
+        response.then().assertThat().statusCode(200);
+
+        //using jsonpath method, we are extracting the value from the response body
+        String firstName = response.jsonPath().getString("employee.emp_firstname");
+        System.out.println(firstName);
+        //first way of assertion
+        Assert.assertEquals(firstName, "sara");
+        //second way of assertion to verify the value in response body using hamcrest matchers
+        response.then().assertThat().body("employee.emp_firstname", equalTo("sara"));
+    }
+
+    @Test
+    public void acreateEmployee(){
+        RequestSpecification request = given().header("Authorization", token)
+                .header("Content-Type", "application/json").
+                body("{\n" +
+                        "  \"emp_firstname\": \"sara\",\n" +
+                        "  \"emp_lastname\": \"bou\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"F\",\n" +
+                        "  \"emp_birthday\": \"2011-01-12\",\n" +
+                        "  \"emp_status\": \"confirmed\",\n" +
+                        "  \"emp_job_title\": \"QA Engineer\"\n" +
+                        "}");
+        Response response = request.when().post("/createEmployee.php");
+        response.prettyPrint();
+        //verfying the statuys code which is 201
+        response.then().assertThat().statusCode(201);
+        //getting the employee id from the response and use it as static one
+        employee_id = response.jsonPath().getString("Employee.employee_id");
+        System.out.println(employee_id);
+        response.then().assertThat().body("Employee.emp_lastname", equalTo("bou"));
+        response.then().assertThat().body("Employee.emp_middle_name", equalTo("ms"));
+        //verify console header
+        response.then().assertThat().header("Server", "Apache/2.4.39 (Win64) PHP/7.2.18");
+
+    }
+
+    @Test
+    public void cupdateEmployee(){
+        RequestSpecification request = given().header("Authorization", token)
+                .header("Content-Type", "application/json").
+                body("{\n" +
+                        "  \"employee_id\": \""+ employee_id +"\",\n" +
+                        "  \"emp_firstname\": \"sohel\",\n" +
+                        "  \"emp_lastname\": \"abbasi\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"M\",\n" +
+                        "  \"emp_birthday\": \"2015-01-12\",\n" +
+                        "  \"emp_status\": \"conf\",\n" +
+                        "  \"emp_job_title\": \"qa\"\n" +
+                        "}");
+
+        Response response = request.when().put("/updateEmployee.php");
+        response.prettyPrint();
+        //verification
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().body("Message", equalTo("Employee record Updated"));
+    }
+
+    @Test
+    public void dgetUpdatedEmployee(){
+        RequestSpecification request = given().header("Authorization", token)
+                .header("Content-Type", "application/json")
+                .queryParam("employee_id", employee_id);
+
+        //to hit the end point/ to make the request which will return response
+        Response response = request.when().get("/getOneEmployee.php");
+
+        // System.out.println(response.asString());
+        response.prettyPrint();
+        //verifying the status code
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().body("employee.emp_job_title", equalTo("QA"));
+    }
+
+}
